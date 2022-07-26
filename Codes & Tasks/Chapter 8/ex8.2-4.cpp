0c1d0fb69065016ba7f9687dbce63abebbcba228
Mohamed Hesham <mohamed.hesham5550@gmail.com>
2022-07-26 04:58:22 +0200

    excercies 8.2-4

---
+++ Codes & Tasks/Chapter 8/ex8.2-4.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k; // number, max
+    cin >> n >> k;
+
+    vector <int> v(n+1);
+
+    vector <int> pre(k+1);
+
+    for(int i = 1; i <= n; i ++) {
+        cin >> v[i];
+        pre[v[i]] ++;
+    }
+
+    for(int i = 1; i <= k; i ++) {
+        pre[i] += pre[i-1];
+    }
+    
+
+    int q;
+    cin >> q;
+    while(q --) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << pre[b] - pre[a-1] << "\n";
+    }
+}
