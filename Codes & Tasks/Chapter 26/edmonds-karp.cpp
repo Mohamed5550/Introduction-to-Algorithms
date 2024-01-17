5929d2c113942d2d893d900cdde31cc107b5b883
Mohamed Hesham <mohamed.hesham5550@gmail.com>
2024-01-17 03:05:46 +0200

    updated edmonds-karp code to be smaller

--- Codes & Tasks/Chapter 26/edmonds-karp.cpp
+++ Codes & Tasks/Chapter 26/edmonds-karp.cpp
@@ -4,100 +4,65 @@ using namespace std;
 
 #define oo 1000000000
 
-vector <vector<int>> E; // edges
 vector <vector<int>> C; // capacities
 vector <vector<int>> F; // flows
-vector <vector<int>> R; // residual network capacities
-vector <vector<int>> G; // adjacensy list
-vector <vector<int>> RG; // residual network as adjacency list
-vector <bool> vs;
-int v, e; // number of vertices and edges
+vector <vector<int>> R; // residual network
+vector <vector<int>> RG; // residual network as ajcaency list
+vector <int> parents; // in shortest paths
+vector <int> distances;
+int v, e; // vertices, edges
 int s, t; // source, sink
-vector <int> pathPrev;
 
-int bfs()
+bool bfs()
 {
-    vs.assign(v+1, 0);
-    queue<pair<int, int>> q; // vertix, prev vertix
-    q.push({s, 0});
-    vector <int> distances(v+1, oo);
-    pathPrev.assign(v+1, -1);
-    distances[s] = 0;
-    while(!q.empty()) {
-        pair<int, int> v = q.front(); q.pop();
-        vs[v.first] = 1;
-        if(distances[v.second] + 1 < distances[v.first]) {
-            distances[v.first] = distances[v.second] + 1;
-            pathPrev[v.first] = v.second;
-        }
-        for(auto i: RG[v.first]) {
-            if(!vs[i]) q.push({i, v.first});
-        }
-    }
-
-    return distances[t];
-}
-
-bool checkResidualNetowrk()
-{
-    RG.assign(v+1, vector<int>());
-    for(int i = 1; i <= v; i ++) {
-        for(auto j: G[i]) {
-            int c1 = C[i][j] - F[i][j];
-            int c2 = F[i][j];
-            
-            if(c1 > 0) {
-                RG[i].push_back(j);
-                R[i][j] = c1;
-            } else {
-                R[i][j] = 0;
-            }
-
-            if(c2 > 0) {
-                RG[j].push_back(i);
-                R[j][i] = c2;
-            } else {
-                R[j][i] = 0;
+    queue<int> q; // vertix
+    distances.assign(v+1, oo);
+    parents.assign(v+1, 0);
+    q.push(s);
+    while(q.size()) {
+        int x = q.front(); q.pop();
+        for(auto i: RG[x]) {
+            if(!R[x][i]) continue;
+            if(distances[i] == oo) {
+                q.push(i);
+                distances[i] = distances[x] + 1;
+                parents[i] = x;
             }
         }
     }
 
-    int sp = bfs(); // shortest path
-
-    return sp != oo;
-}
-
-int getResidualCapacity()
-{
-    int c = oo;
-    int i = t;
-    while(i != s) {
-        c = min(c, R[pathPrev[i]][i]);
-        i = pathPrev[i];
-    }
-    return c;
+    return distances[t] != oo;
 }
 
 int EdmondsKarp()
 {
-    while(checkResidualNetowrk()) { // there is an augmentation path in the residual network
-        int c = getResidualCapacity(); // min capacity in the path
-        int i = t;
-        while(i != s) {
-            c = min(c, R[pathPrev[i]][i]);
-            int a = pathPrev[i];
-            int b = i;
-            if(E[a][b]) F[a][b] += c;
-            else F[a][b] -= c;
-            
-            i = pathPrev[i];
+    while(bfs()) {
+        // augment by shortest path and make residual network
+        int c = oo; // residual capacity
+        auto it = t;
+        while(it != s) {
+            c = min(c, R[parents[it]][it]);
+            it = parents[it];
+        }
+        
+        it = t;
+        while(it != s) {
+            if(C[parents[it]][it]) {
+                F[parents[it]][it] += c;
+                R[parents[it]][it] = C[parents[it]][it] - F[parents[it]][it];
+                R[it][parents[it]] = F[parents[it]][it];
+            } else {
+                F[it][parents[it]] -= c;
+                R[parents[it]][it] = F[it][parents[it]];
+                R[it][parents[it]] = C[it][parents[it]] - F[it][parents[it]];
+            }
+            it = parents[it];
         }
     }
 
     int maxFlow = 0;
     for(int i = 1; i <= v; i ++) {
-        maxFlow += F[s][i];
-        maxFlow -= F[i][s];
+        maxFlow = maxFlow + F[s][i] - F[i][s];
     }
 
     return maxFlow;
@@ -107,18 +72,18 @@ int main()
 {
     cin >> v >> e;
     cin >> s >> t;
-    E.assign(v+1, vector<int>(v+1));
     C.assign(v+1, vector<int>(v+1));
-    F.assign(v+1, vector<int>(v+1));
     R.assign(v+1, vector<int>(v+1));
-    G.assign(v+1, vector<int>());
+    F.assign(v+1, vector<int>(v+1));
+    RG.assign(v+1, vector<int>());
 
     // getting edges and capacities from user
     for(int i = 1; i <= e; i ++) {
         int a, b, c; cin >> a >> b >> c;
-        E[a][b] = 1;
-        C[a][b] = c;
-        G[a].push_back(b);
+        C[a][b] += c;
+        R[a][b] += c;
+        RG[a].push_back(b);
+        RG[b].push_back(a);
     }
 
     cout << "Max flow is: " << EdmondsKarp() << "\n";
